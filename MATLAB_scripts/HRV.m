de35d4582b190d48f36f811d987e7345bb2488a5
jose <jose.barriosr@sansano.usm.cl>
2023-10-03 21:30:53 -0300

    * FIX: plotting with wrong array

--- MATLAB_scripts/HRV.m
+++ MATLAB_scripts/HRV.m
@@ -1,17 +1,3 @@
-%%
-newMatrix = zeros(length(s)/6,6);
-j = 1;
-
-for i = 1:length(s)/6
-    newMatrix(i,:) = s(j:j+5);
-    j = j+6;
-end
-%%
-newMatrix = [s(1:30000) s(1:30000) s(1:30000) s(1:30000) s(1:30000) s(1:30000)];
-
-%writematrix(newMatrix,'.tsv','Delimiter','\t','FileType','text');
-writematrix(newMatrix,'ECG_arti_wstd_wnoise.tsv','Delimiter','\t','FileType','text');
-
 %% Ground Truth
 L = length(ipeaks);
 t = linspace(0,N,L);
@@ -28,8 +14,9 @@ for i = 1:L
     end
 end
 
-intervals = diff(pre_intervals)/sfecg -1;
+intervals = diff(pre_intervals)/sfecg;
 %heart_rate = 60.*intervals/sfecg;
+
 %%
 RR0 = zeros(N,1);
 RR1 = zeros(N,1);
@@ -41,10 +28,11 @@ RR6 = zeros(N,1);
 RR7 = zeros(N,1);
 
 RR_buffer = [RR0 RR1 RR2 RR3 RR4 RR5 RR6 RR7];
+
 %% Leer archivos r_peaks r_ts
 
 for i = 0:7
-    path = strcat('RR_peaks/RR_peaks_no_no_60/RR_',string(i),'.txt');
+    path = strcat('RR_peaks/RR_peaks_w_w_60/RR_',string(i),'.txt');
     aux = readtable(path,'Delimiter','\t');
     aux = table2array(aux);
     aux_L = length(aux);
@@ -54,14 +42,15 @@ end
 figure()
 plot(tp,intervals)
 hold on
-plot(RR0)
-plot(RR1)
-plot(RR2)
-plot(RR3)
-plot(RR4)
-plot(RR5)
-plot(RR6)
-plot(RR7)
+plot(RR_buffer(:,1))
+plot(RR_buffer(:,2))
+plot(RR_buffer(:,3))
+plot(RR_buffer(:,4))
+plot(RR_buffer(:,5))
+plot(RR_buffer(:,6))
+plot(RR_buffer(:,7))
+plot(RR_buffer(:,8))
 hold off
 xlim([5 30])
 legend('True HR','Two Average','Matched Filter','SWT','Engzee','Christov','Hamilton','Pan Tompkins','WQRS')
+
