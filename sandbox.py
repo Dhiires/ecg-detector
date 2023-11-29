7f428bae8322f8731b1e2d033846cced9629c064
jose <jose.barriosr@sansano.usm.cl>
2023-11-28 21:32:56 -0300

    * CHG: renames files

--- sandbox.py
+++ sandbox.py
@@ -1,14 +1,54 @@
-import numpy as np
 import pathlib
-from ecgdetectors import Detectors
 import os
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 
-def GetPath():
-    initial_dir = os.getcwd()
-    Tk().withdraw()
-    path = askopenfilename(initialdir=initial_dir)
-    return path
+########################## INIT FUNCTIONS ##########################
 
-print(GetPath())
+def RunMethods(experiment, subject):    
+    for i in range(8):
+        pre_folder_path = current_dir.parent / 'DB_RR_intervals'
+        specific_folder_path = 'RR_' + experiment + '_' + subject
+        rr_folder_path = pre_folder_path / specific_folder_path
+        
+        if not os.path.exists(rr_folder_path):
+            os.makedirs(rr_folder_path)
+            
+    return rr_folder_path
+
+########################## END FUNCTIONS ##########################
+
+current_dir = pathlib.Path(__file__).resolve()
+parent_dir = current_dir.parent
+
+data_dir = parent_dir / 'dataset_716' / 'experiment_data'
+
+current_list = os.listdir(data_dir)
+
+for subject in current_list:
+    display_string = 'Reading ' + subject
+    print(display_string)
+    
+    sub_list = os.listdir(data_dir / subject)
+    for experiment in sub_list:
+        if experiment == 'hand_bike':
+            display_string = 'Experiment ' + experiment
+            print(display_string)
+
+            rr_folder_path = RunMethods(experiment,subject)
+        
+            wrong_file = 'annotation_' + experiment + '_' + subject + '.txt'
+            wrong_file_path = rr_folder_path / wrong_file
+
+            right_file = 'annotation_' + 'handbike_' + subject + '.txt'
+            right_file_path = rr_folder_path / right_file
+            
+            pre_folder_path = current_dir.parent / 'DB_RR_intervals'
+            right_folder_name = 'RR_' + 'handbike_' + subject
+            right_folder_path = pre_folder_path / right_folder_name
+            
+            if os.path.exists(wrong_file_path):
+                os.rename(wrong_file_path, right_file_path)
+                
+            if os.path.exists(rr_folder_path):
+                os.rename(rr_folder_path,right_folder_path)
+    
+print("Finished")
