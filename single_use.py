96844a11b38a81a8cbc921764d697d6c592901c5
jose <jose.barriosr@sansano.usm.cl>
2023-12-11 23:33:22 -0300

    * CHG: added detection

--- single_use.py
+++ single_use.py
@@ -20,23 +20,29 @@ fs = 250
 
 detectors = Detectors(fs)
 
-title = "Detección de onda R"
+title = "Detección onda R y Ground Truth"
 metodo = 0
-r_peaks = detectors.get_detector_list()[metodo][1](unfiltered_ecg)
+
+r_peaks_detected = detectors.get_detector_list()[metodo][1](unfiltered_ecg)
+r_ts_d = np.array(r_peaks_detected) / fs
+r_peaks = np.loadtxt(GetPath())
+r_peaks = r_peaks.astype(int)
+r_peaks = [n-1 for n in r_peaks]
 r_ts = np.array(r_peaks) / fs
 
 plt.figure()
 t = np.linspace(0, len(unfiltered_ecg) / fs, len(unfiltered_ecg))
 plt.plot(t,unfiltered_ecg)
-plt.plot(r_ts, unfiltered_ecg[r_peaks],'ro')
-plt.xlim(20,30)
+plt.plot(r_ts, unfiltered_ecg[r_peaks],'go')
+plt.plot(r_ts_d, unfiltered_ecg[r_peaks_detected],'ro')
+plt.xlim(110,120)
 plt.title(title)
 plt.ylabel("ECG (mV)")
 plt.xlabel("Time (s)")
 plt.show()
 
-plt.plot(t,unfiltered_ecg)
-plt.title(title)
-plt.ylabel("ECG (mV)")
-plt.xlabel("Time (s)")
-plt.show()
+#plt.plot(t,unfiltered_ecg)
+#plt.title(title)
+#plt.ylabel("ECG (mV)")
+#plt.xlabel("Time (s)")
+#plt.show()
